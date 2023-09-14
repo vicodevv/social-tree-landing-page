64a82b85d7b852f833cd772a41804d6dae23b8ca
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-09-14 19:47:57 +0100

    Added Arrow to HomePage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,24 @@
 import './Home.scss'
 import React from 'react'
 import Header from '../../components/Header'
+import arrow from '../../assets/images/arrow.png'
+import arrow2 from '../../assets/images/arrow2.png'
 
 const Home = () => {
     return (
         <div>
             <Header />
             <div className="home">
-                <div className="home__title">
+                <div className="home__hero">
+                <div className="image">
+                    <img src={arrow2} alt='arrow'/>
+                </div>
+                <div className="home__hero__title">
                     MANAGE SOCIALS EASILY
+                </div>
+                <div className="image">
+                    <img src={arrow} alt='arrow'/>
+                </div>
                 </div>
                 <div className="home__fact">
                     <div className="home__fact__title">
@@ -16,7 +26,7 @@ const Home = () => {
                         </div>
                     <div className="home__fact__content">
                         Social Media is a great way to connect with friends and family, but it can also be a great way to connect with customers and clients.
-                        </div>
+                    </div>
                 </div>
             </div>
         </div>
